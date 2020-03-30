9f2a026702518d692850a5830c7ae575ae30a9b1
Karl Kristjánsson <Karlk15@ru.is>
2020-03-30 23:02:23 +0000

    Update ViewController.swift

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -1,10 +1,6 @@
-//
 //  ViewController.swift
 //  TicTacToe
-//
-//  Created by Karl Kristjánsson on 27/03/2020.
-//  Copyright © 2020 Karl Kristjánsson. All rights reserved.
-//
+
 
 import UIKit
 
